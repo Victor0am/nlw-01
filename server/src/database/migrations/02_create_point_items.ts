9174eb9a7860f56077000c2be863e0d7e0920caf
Victor Aguiar Marques <victoramarques@hotmail.com>
2020-06-05 16:24:46 -0300

    App

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+//criar a tabela
+export async function up(knex: Knex){
+    return knex.schema.createTable('point_items', table =>{
+        table.increments('id').primary();
+
+        table.integer('point_id').notNullable().references('id').inTable('points');
+        table.integer('item_id').notNullable().references('id').inTable('items');
+        
+    });
+};
+//deletar a tabela
+export async function down(knex: Knex){
+    return knex.schema.dropTable('point_items');
+};

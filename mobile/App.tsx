c31c18c28f14154d12263500a6e6c4cf974e6f0c
Victor Aguiar Marques <victoramarques@hotmail.com>
2020-06-07 12:40:41 -0300

    Correção do :bug: das fontes, e inclusão do picker para escolher cidade/estado

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { AppLoading } from 'expo';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import  { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import  { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import { useFonts } from '@use-expo/font';
+
 
 import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Ubuntu_700Bold,
+    'Roboto_400Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
+    'Roboto_500Medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
+    'Ubuntu_700Bold': require('./assets/fonts/Ubuntu/Ubuntu-Bold.ttf'),
   });
 
   if (!fontsLoaded) {
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,24 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Platform, Text, Image, ImageBackground, TextInput, KeyboardAvoidingView } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native'
 import styles from './styles';
+import { Picker } from '@react-native-community/picker'
+import axios from 'axios';
 
 
 // import { Container } from './styles';
+interface IBGEUFResponse {
+    sigla: string
+}
+
+interface IBGECityResponse {
+    nome: string
+}
 
 const Home = () => {
     const navigation = useNavigation();
     const [uf, setUf] = useState("");
     const [city, setCity] = useState("");
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
     function handleNavigateToPoints() {
         navigation.navigate('Points', {
             city, uf
         });
     }
+
+     useEffect(()=> {
+         axios.get<IBGEUFResponse[]>("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+         .then(response => {
+             console.log("AAAAAAAAAAA");
+             const ufInitials = response.data.map(uf => uf.sigla);
+             ufInitials.sort((uf1,uf2)=> uf1.localeCompare(uf2));
+             setUfs(ufInitials);
+         });
+
+     }, []);
+
+     useEffect(()=>{
+        if(uf === ''){
+            return;
+        }
+
+         axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+         .then(response => {
+             const cityNames = response.data.map(city => city.nome);
+             setCities(cityNames);
+         });
+     }, [uf]);
+
+
+    // function handleSelectUf(event: <HTMLSelectElement>){
+    //     const uf = event.target.value;
+    //     setSelectedUf(uf);
+    // }
+
+    // function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
+    //     const city = event.target.value;
+    //     setSelectedCity(city);
+    // }
+
+
     return (
-        <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS === 'ios'? 'padding': undefined}>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
             <ImageBackground
                 style={styles.container}
                 source={require('../../assets/home-background.png')}
@@ -27,11 +74,35 @@ const Home = () => {
                 <View style={styles.main}>
                     <Image source={require('../../assets/logo.png')} />
                     <View>
-                    <Text style={styles.title} >Seu marketplace de coleta de resíduos</Text>
-                    <Text style={styles.description}> Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+                        <Text style={styles.title} >Seu marketplace de coleta de resíduos</Text>
+                        <Text style={styles.description}> Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
                     </View>
                 </View>
                 <View style={styles.footer}>
+
+
+                    <Picker
+                        selectedValue={uf}
+                        onValueChange={uf => setUf(String(uf))}
+                        style={styles.input}
+                        mode="dropdown"
+                    >
+                        <Picker.Item label="Escolha uma UF" value="" key={0}/>
+                        {ufs.map((uf, index)=> {
+                            return (<Picker.Item label={uf} value={uf} key={index+1}/>)
+                        })}
+                    </Picker>
+                    <Picker
+                        selectedValue={city}
+                        onValueChange={city => setCity(String(city))}
+                        style={styles.input}
+                        mode="dropdown"
+                    >
+                        <Picker.Item label="Escolha uma cidade" value="" key={0}/>
+                        {cities.map((city, index)=> {
+                            return (<Picker.Item label={city} value={city} key={index+1}/>)
+                        })}
+                    </Picker>
                     <TextInput
                         style={styles.input}
                         placeholder="Digite a UF"
@@ -40,7 +111,7 @@ const Home = () => {
                         autoCapitalize='characters'
                         autoCorrect={false}
                         onChangeText={setUf} />
-                        
+
                     <TextInput style={styles.input}
                         placeholder="Digite a Cidade"
                         value={city}
